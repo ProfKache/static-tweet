2784bf8ddd713b3ae11fb6e66b513a12df3cb871
Salim Kachemela <sakachemela@gmail.com>
2020-05-08 14:32:32 +0300

    Finalize static tweet V1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import avatar from './images/profile.png';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Tweet = () => (
+  <div className="tweet">
+    <Avatar />
+    <div className="content">
+      <Author />
+      <Time />
+      <Message />
+      <div className="buttons">
+        <ReplyButton />
+        <RetweetButton />
+        <LikeButton />
+        <ShareButton />
+      </div>
+    </div>
+  </div>
+); 
+
+const Avatar = () => (
+  <img src={avatar} className="avatar" alt="avatar" />
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Message = () => (
+  <div className="message">
+    React is really awesome, epspecially when mixing with vim
+  </div>
+);
+
+const Author = () => (
+  <span className="author">
+    <span className="name">Salim Kachemela</span>
+    <span className="handle">@ProfKache</span>
+  </span>
+);
+
+const Time = () => (
+  <span className="time">3h ago</span>
+);
+
+const ReplyButton = () => (
+  <i className="fas fa-reply reply-button" />
+);
+
+const RetweetButton = () => (
+  <i className="fas fa-retweet retweet-button" />
+);
+
+const LikeButton = () => (
+  <i className="far fa-heart like-button" />
+);
+
+const ShareButton = () => (
+  <i className="fas fa-share-alt share-button" />
+);
+
+
+ReactDOM.render(<Tweet />, document.querySelector('#root'));
